8a8fc0519444850dabd247387b13e813b0c0c253
Francesca Andrian <francescaandrianaaa@gmail.com>
2025-03-15 17:45:46 +0200

    Modify the commit.

--- Test2 Andrian Francesca/src/Ex14.java
+++ Test2 Andrian Francesca/src/Ex14.java
@@ -1,3 +1,10 @@
 public class Ex14 {
+    public static void main(String[] args) {
 
+        try {
+            throw new Exception("Aceasta este o eroare!");
+        } catch (Exception error) {
+            System.out.println(error.getMessage());
+        }
+    }
 }
